1afa025d86159dfa782643ef5eb0855c794cdafd
chosenbreed37 <chosenbreed@gmail.com>
2017-09-23 21:28:31 +0100

    Added a few more quotes. Tweaked running script

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 docker rm -f quotes-db-mongo
 docker run -d --name quotes-db-mongo -p 27017:27017 --volume $(pwd):/tmp/data mongo mongod
 docker exec -it quotes-db-mongo mongoimport --verbose --db quotes --collection quotes --type tsv --headerline --file /tmp/data/quotes.tsv --drop
-
+echo "Quotes database running on  on port 27017..."
