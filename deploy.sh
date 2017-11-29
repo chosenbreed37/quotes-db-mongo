d898e1e9ad525a56ad04411d64ba02d920f484bf
Eric Lubisse <ericl@digiterre.com>
2017-11-29 22:39:14 +0000

    Changing deployment script to run docker with elevated privileges

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd apps
 rm -rf quotes-db-mongo
 git clone https://github.com/chosenbreed37/quotes-db-mongo.git
 cd quotes-db-mongo
-docker rm -f quotes-db-mongo
-docker run -d --name quotes-db-mongo -p 27017:27017 --volume $(pwd):/tmp/data mongo mongod
-docker exec -it quotes-db-mongo mongoimport --verbose --db quotes --collection quotes --type tsv --headerline --file /tmp/data/quotes.tsv --drop
+sudo docker rm -f quotes-db-mongo
+sudo docker run -d --name quotes-db-mongo -p 27017:27017 --volume $(pwd):/tmp/data mongo mongod
+sudo docker exec -it quotes-db-mongo mongoimport --verbose --db quotes --collection quotes --type tsv --headerline --file /tmp/data/quotes.tsv --drop
 echo "Quotes database running on  on port 27017..."
